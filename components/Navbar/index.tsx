97221e81dc6a6f69a6e79148edfc045e42a3a02a
eren-olympic <eren.olympic.14@gmail.com>
2023-05-12 01:17:29 +0800

    fix: padding issue

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     const theme = useTheme();
 
     return (
-        <Box bg={theme.colors.background} w="100%" borderBottom='1px solid' p={4} color={theme.colors.comment}>
+        <Box bg={theme.colors.background} w="100%" borderBottom='1px solid' px='8' py='4' color={theme.colors.comment}>
             <Box as='button' borderRadius='md' bg={theme.colors.primary} color={theme.colors.text} p={2} _hover={{ bg: theme.colors.primaryDark }}>
                 Neobase
             </Box>
